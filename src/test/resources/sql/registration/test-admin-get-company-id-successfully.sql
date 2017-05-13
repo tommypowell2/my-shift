784644b2528ddeea897e420ca64996157229a6fb
root <tommypowell2@gmail.com>
2017-05-13 13:35:26 +0000

    SCHED-39 Fixed test

--- src/test/resources/sql/registration/test-admin-get-company-id-successfully.sql
+++ src/test/resources/sql/registration/test-admin-get-company-id-successfully.sql
@@ -1,8 +1,14 @@
+drop table authorities if EXISTS;
+CREATE TABLE authorities
+(
+    username VARCHAR(255),
+    authority VARCHAR(255)
+);
 CREATE UNIQUE INDEX authorities_username_authority_key ON authorities (username, authority);
 
 insert into shift_user (username, password,companyid, enabled) VALUES ('username', 'sasa', 1, TRUE );
-CREATE UNIQUE INDEX users_companyID ON shift_user (username, companyid);
-
+--CREATE UNIQUE INDEX users_companyID ON shift_user (username, companyid);
+drop table company if EXISTS;
 CREATE TABLE company
 (
     id INTEGER IDENTITY PRIMARY KEY NOT NULL,
